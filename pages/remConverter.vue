5753dfbb049734dfee10c4a1cf035cdecdd0c0c3
SokvisalMong <sokvisalmong@gmail.com>
2024-12-28 23:20:14 +0700

    temp rem converter page

--- pages/remConverter.vue
+++ pages/remConverter.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    Rem
+  <div class="min-h-screen flex justify-center items-center">
+    <div class="border-2 border-frost-200 rounded-lg shadow-2xl p-4 bg-polar-night-200">
+      Rem Converter
+    </div>
   </div>
 </template>
