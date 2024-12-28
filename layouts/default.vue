7cd6033334f2499eb89f7c5015a58f7308861628
SokvisalMong <sokvisalmong@gmail.com>
2024-12-28 21:47:26 +0700

    removed navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="min-h-screen w-screen h-full bg-polar-night-300 text-snow-storm-300">
-    <AppNavbar/>
     <NuxtPage/>
   </div>
 </template>
---
+++ pages/remConverter.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Rem
+  </div>
+</template>
