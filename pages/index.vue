018c30ad7a4dde94fef1c4811dc35f6f629dc439
SokvisalMong <sokvisalmong@gmail.com>
2024-12-28 23:45:25 +0700

    added shadnc

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,13 @@
         <div v-for="page in pages" class="p-4">
           <NuxtLink :href="page.link" class="flex items-center hover:text-snow-storm-300">
             <span>{{ page.label }}</span>
-            <Icon :name="page.icon" class="ml-2"/>
+            <Icon :name="page.icon" class="ml-2" size="20"/>
           </NuxtLink>
         </div>
         <div>
           <NuxtLink href="https://github.com/SokvisalMong/utilities" target="_blank" rel="noopener noreferrer" class="flex items-center text-snow-storm-300 hover:text-snow-storm-100">
             <Icon name="mdi:github" size="24" class="mr-2"/>
-            <span>by Sokvisal Mong</span>
+            <span>Github</span>
           </NuxtLink>
         </div>
       </div>
