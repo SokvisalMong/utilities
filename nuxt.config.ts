a54cbc3599d12f5fe5fb4739a3ade4902cc06262
SokvisalMong <sokvisalmong@gmail.com>
2024-12-29 00:05:09 +0700

    remvoed headlessui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon', 'nuxt-headlessui'],
-  headlessui: {
-    prefix: 'Headless'
-}
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon']
 })
--- pages/remConverter.vue
+++ pages/remConverter.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="min-h-screen flex justify-center items-center">
-    <div class="border-2 border-frost-200 rounded-lg shadow-2xl p-4 bg-polar-night-200">
+    <div class="relative border-2 border-frost-200 rounded-lg shadow-2xl p-8 bg-polar-night-200">
+      <div class="absolute top-2 right-2">
+        <RemConverterConfig/>
+      </div>
       Rem Converter
     </div>
   </div>
