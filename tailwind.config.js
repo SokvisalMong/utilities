916f36e95f710a4ec28b3f062c76dd2adc4fc81b
SokvisalMong <sokvisalmong@gmail.com>
2024-12-28 21:29:29 +0700

    added nordtheme colors

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+export default { 
+  theme: {
+    extend: {
+      colors: {
+        'polar-night': {
+          100: '#4C566A',
+          200: '#434C5E',
+          300: '#3B4252',
+          400: '#2E3440'
+        },
+        'snow-storm': {
+          100: '#ECEFF4',
+          200: '#E5E9F0',
+          300: '#D8DEE9'
+        },
+        'frost': {
+          100: '#5E81AC',
+          200: '#81A1C1',
+          300: '#88C0D0',
+          400: '#8FBCBB'
+        },
+        'aurora': {
+          'red': '#BF616A',
+          'orange': '#D08770',
+          'yellow': '#EBCB8B',
+          'green': '#A3BE8C',
+          'magenta': '#B48EAD'
+        }
+      }
+    }
+  }
+}
