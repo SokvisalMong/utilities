6ec57e5b551aaea4e454230e4817bf14285c06d5
SokvisalMong <sokvisalmong@gmail.com>
2024-12-28 22:39:10 +0700

    added home button

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="min-h-screen w-screen h-full bg-polar-night-300 text-snow-storm-300">
+    <div class="fixed top-4 left-4" :class="[showHomeButton ? 'block' : 'hidden']">
+      <NuxtLink href="/">
+        <div class="flex items-center"> 
+          <Icon name="mdi:arrow-back" class="translate-y-px mr-1"/>
+          <span class="text-xl">Home</span>
+        </div>
+      </NuxtLink>
+    </div>
     <NuxtPage/>
   </div>
 </template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+const showHomeButton = computed(() => route.path !== '/')
+</script>
