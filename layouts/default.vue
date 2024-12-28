5174d97a599070d635ab63b984fdf6698ed2586a
SokvisalMong <sokvisalmong@gmail.com>
2024-12-28 21:44:27 +0700

    updated text color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="min-h-screen w-screen h-full bg-polar-night-300">
+  <div class="min-h-screen w-screen h-full bg-polar-night-300 text-snow-storm-300">
+    <AppNavbar/>
     <NuxtPage/>
   </div>
 </template>
